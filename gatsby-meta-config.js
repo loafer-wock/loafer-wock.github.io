4675be4373a950d5fef53a50ca97e38308a1b6ef
loafer <jtret2423@gamil.com>
2023-08-24 17:23:37 +0900

    gitupload

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -43,8 +43,26 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2023.08 ~',
-        activity: '블로그 생성',
+        date: '2018.10 ~ 2020.10',
+        activity: 'Netmarble(Seoul, South Korea) / Security Solution Engineer',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/loafer-wock/loafer-wock.github.io',
+          demo: 'https://github.com/loafer-wock',
+        },
+      },
+      {
+        date: '2020.12 ~ 2022.12',
+        activity: 'Cloudgram(Seoul, South Korea) / AWS Solutions Architect',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/loafer-wock/loafer-wock.github.io',
+          demo: 'https://github.com/loafer-wock',
+        },
+      },
+      {
+        date: '2023.01 ~ ',
+        activity: 'Megazone Cloud(Seoul, South Korea) / AWS Solutions Architect',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
           github: 'https://github.com/loafer-wock/loafer-wock.github.io',
@@ -70,19 +88,20 @@ module.exports = {
         },
       },
       // ========================================================
+      // About의 Project 부분
       // ========================================================
-      {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
+      //{
+      //  title: '개발 블로그 테마 개발',
+      //  description:
+      //    '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
+      //  techStack: ['gatsby', 'react'],
+      //  thumbnailUrl: 'blog.png',
+      //  links: {
+      //    post: '/gatsby-starter-zoomkoding-introduction',
+      //    github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+      //    demo: 'https://www.zoomkoding.com',
+      //  },
+      //},
     ],
   },
 };

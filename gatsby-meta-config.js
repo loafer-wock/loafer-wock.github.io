87257bd25a95032e40b1a2df988c7c97d5632484
loafer <jtret2423@gamil.com>
2023-08-25 13:24:27 +0900

    20230825

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: `loafer-wock.github.io`, //이게 타이틀~
-  description: `욱현의 기술블로그`,
+  description: `욱현이의 기술블로그`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://loafer-wock.github.io`,
   ogImage: `/memoji_wock.png`, // Path to your in the 'static' folder
@@ -99,18 +99,18 @@ module.exports = {
       // ========================================================
       // About의 Project 부분
       // ========================================================
-      //{
-      //  title: '개발 블로그 테마 개발',
-      //  description:
-      //    '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-      //  techStack: ['gatsby', 'react'],
-      //  thumbnailUrl: 'blog.png',
-      //  links: {
-      //    post: '/gatsby-starter-zoomkoding-introduction',
-      //    github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-      //    demo: 'https://www.zoomkoding.com',
-      //  },
-      //},
+      {
+        title: '프로젝트',
+        description:
+          'AWS EKS를 활용한 뭐시깽이',
+        techStack: ['AWS Cloud1', 'Kubernetes'],
+        thumbnailUrl: '',
+        links: {
+          post: '',
+          github: '',
+          demo: '',
+        },
+      },
     ],
   },
 };
